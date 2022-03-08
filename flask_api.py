c7346df52c2b7cb787b5bae64c93eac437cda656
Ithalorc <ithalorc@Vulcan-3000.localdomain>
2022-03-08 19:59:40 -0300

    Primeiro commit com arquivo inicial do projeto em Python. :seedling:

---
+++ flask_api.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 14:35:13 2022
+
+@author: ithal
+"""
+
+from flask import Flask, request
+from flask_pydantic_spec import FlaskPydanticSpec, Response, Request
+from pydantic import BaseModel
+import json
+
+#Indicando que a aplicação usará o flask
+server = Flask (__name__)
+
+#Especificação da minha API, chamada de api
+api = FlaskPydanticSpec('flask', title = 'Super API no Swagger! :D')
+
+#Classe Qa importando BaseModel da biblioteca Pydantic
+class Qa(BaseModel):
+    nome: str
+    
+api.register(server) #Minha especificação será do meu servidor // Vai registrar os endpoints do meu servidor
+
+# Pegar algo do servidor
+@server.get('/') #Rota, endpoint
+def QA_devs():
+    """Informa os QAs que fizeram essa página"""
+    return 'Ithalo e Rafael'
+
+@server.post('/')
+@api.validate(body=Request(Qa))#, resp=Response(HTTP_201=Qa))
+def envia_nome():
+    """ Verifica Nome para informar sobrenome"""
+    x = request.context.body.dict()
+    #Não consegui entender bem a sintaxe do body, então converti o json gerado para conseguir validar o nome obtido
+    y = json.dumps(x)
+    
+    if '"Ithalo"' in y:
+        return 'Conceição'
+    elif '"Rafael"' in y:
+        return 'Amodio'
+    else:
+        return 'QA Inválido.'
+
+server.run()
